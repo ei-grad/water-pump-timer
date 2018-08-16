8d14b83d16950261637e2e70b1a752cd40fd4b46
Andrew Grigorev <andrew@ei-grad.ru>
2018-08-16 18:00:38 +0300

    Don't fail boot.py on WiFi failure

--- boot.py
+++ boot.py
@@ -1,16 +1,25 @@
 import gc
 import webrepl
 import network
+from time import sleep
 
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.scan()
-
-with open('wifi-creds.txt') as f:
-    ssid, passwd = f.read().strip().split(':')
-
-wlan.connect(ssid, passwd)
+try:
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.scan()
+    with open('wifi-creds.txt') as f:
+        ssid, passwd = f.read().strip().split(':')
+    wlan.connect(ssid, passwd)
+    for i in range(10):
+        sleep(1)
+        if wlan.ifconfig()[0] != '0.0.0.0':
+            break
+    else:
+        print("DHCP not recieved!")
+    print("Wifi '%s' connected. IP Address: %s" % (ssid, wlan.ifconfig()[0]))
+except Exception as e:
+    print("Can't connect to WiFi: %s", e)
 
 webrepl.start()
 
