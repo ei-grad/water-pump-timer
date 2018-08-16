b8648f501967e275bb863c27bc484ec69c08a73f
Andrew Grigorev <andrew@ei-grad.ru>
2018-08-16 17:59:40 +0300

    Timer version

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
-from machine import Pin
-from time import sleep
+from machine import Pin, Timer
+import micropython
 
 
 p0 = Pin(0, Pin.OUT, value=1)
 
 
-def get_duration():
-    with open('duration.txt', 'r') as f:
+def read_int(filename):
+    with open(filename, 'r') as f:
         return int(f.read())
 
 
-def get_interval():
-    with open('interval.txt', 'r') as f:
-        return int(f.read())
+timer = Timer(0)
+counter = 0
 
 
-for i in range(10):
-    p0.value(0)  # value 0 - turn on
-    sleep(get_duration())
-    p0.value(1)  # value 1 - turn off
-    sleep(get_interval())
+def turn_on(t):
+    global counter
+    counter += 1
+    if counter >= 10:
+        return
+    print("Pump: on")
+    p0.value(0)
+    timer.init(mode=Timer.ONE_SHOT, period=read_int('duration.txt') * 1000, callback=turn_off_irq)
+
+
+def turn_off(t):
+    print("Pump: off")
+    p0.value(1)
+    timer.init(mode=Timer.ONE_SHOT, period=read_int('interval.txt') * 1000, callback=turn_on_irq)
+
+
+def turn_on_irq(t):
+    micropython.schedule(turn_on, t)
+
+
+def turn_off_irq(t):
+    micropython.schedule(turn_off, t)
+
+
+turn_on(0)
