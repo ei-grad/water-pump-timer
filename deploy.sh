4c319d77f9cee5e49e324fa3b54608b8d7ea95c5
Andrew Grigorev <andrew@ei-grad.ru>
2018-08-19 08:27:27 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 set -e
 set -x
 
+rm *.mpy
+
 for i in `ls *.py | grep -v main.py`; do
     ~/repos/micropython/mpy-cross/mpy-cross $i
 done
 
-for i in *.mpy; do
+for i in *.mpy webform.tpl main.py; do
     webrepl_cli -p $WEBREPL_PASSWD $i 10.12.1.99:/$i
 done
-
-webrepl_cli -p $WEBREPL_PASSWD main.py 10.12.1.99:/main.py
